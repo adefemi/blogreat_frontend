bcab0c3ef78ed1f1290ccd6b912c96e965e5cd4b
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-05-30 11:35:16 +0100

    implement dropdown component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,20 @@
+"use client"
+
+import useDropdown from "./dropdown"
+
 const Search = () => {
-    return <div className="search">
-        <input placeholder="search" />
+    const {wrapperRef, setOpen, setClose, getDropdown} = useDropdown()
+    return <div className="search" ref={wrapperRef}>
+        <input placeholder="search" onFocus={() => setOpen()} />
+        {
+            getDropdown(<div>
+
+                {
+                    [1,2,4,5].map((item, index) => <li key={index}>{item}</li>)
+                }
+
+            </div>)
+        }
     </div>
 }
 
---
+++ components/dropdown.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from "react"
+
+const useDropdown = () => {
+    const wrapperRef = useRef<any>()
+
+    const getDropdownElement = () => {
+        return document.getElementById("dropDownContainer")
+    }
+
+    const hasCloseClass = () => {
+        return getDropdownElement()?.classList.contains("close")
+    }
+
+    const setOpen = () => {
+        if(!hasCloseClass())return
+        getDropdownElement()?.classList.remove("close")
+    }
+
+    const setClose = () => {
+        if(hasCloseClass())return
+        getDropdownElement()?.classList.add("close")
+    }
+
+    const handleOutsideClick = (event:any) => {
+        if(wrapperRef.current && !wrapperRef.current.contains(event.target)){
+            setClose()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick)
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick)
+        }
+    }, [])
+
+    const getDropdown = (children: React.ReactNode) => {
+        return <div className="dropdown close" id="dropDownContainer">
+            {children}
+        </div>
+    }
+
+    return {
+        getDropdown,
+        wrapperRef,
+        setOpen,
+        setClose
+    }
+}
+
+export default useDropdown
