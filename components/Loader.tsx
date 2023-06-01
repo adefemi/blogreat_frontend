fa3e40347b4cbaf7fa446082defecedfb15655ea
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-06-01 08:13:16 +0100

    integrate search

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+import {AiOutlineLoading} from "react-icons/ai"
+
+const Loader = () => {
+    return <div className="loader">
+        <AiOutlineLoading />
+    </div>
+}
+
+export default Loader
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,21 +1,76 @@
-"use client"
+"use client";
 
-import useDropdown from "./dropdown"
+import { useEffect, useRef, useState } from "react";
+import Loader from "./Loader";
+import useDropdown from "./dropdown";
+import { IBlog } from "./BlogCard";
+import { getData } from "@/utils/server";
 
 const Search = () => {
-    const {wrapperRef, setOpen, setClose, getDropdown} = useDropdown()
-    return <div className="search" ref={wrapperRef}>
-        <input placeholder="search" onFocus={() => setOpen()} />
-        {
-            getDropdown(<div>
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef<any>();
+  const [searchBlogs, setSearchBlogs] = useState<IBlog[]>([]);
+  let debounce: any;
 
-                {
-                    [1,2,4,5].map((item, index) => <li key={index}>{item}</li>)
-                }
+  const onChange = (e: any) => {
+    clearTimeout(debounce);
+    debounce = setTimeout(() => {
+      setSearch(inputRef.current?.value);
+    }, 1000);
+  };
 
-            </div>)
-        }
+  const makeSearch = async () => {
+    setLoading(true)
+    const blogs = (await getData<IBlog[]>(`blog-url?keyword=${search}`)) || [];
+    setLoading(false)
+    setSearchBlogs(blogs)
+  }
+
+  useEffect(() => {
+    if(search){
+      makeSearch()
+    }
+  }, [search]);
+
+  const getSearchInfo = () => {
+
+    if(!search){
+      return <div className="placeholder">Start typing</div>
+    }
+
+    if(loading){
+      return <Loader />
+    }
+
+    if(searchBlogs.length > 0){
+      return searchBlogs.map((item, index) => (
+        <div className="searchItem" key={index}>
+          {item.title}
+        </div>
+      ))
+    }
+    else{
+      return <div className="notFound">No blog found</div>
+    }
+  }
+
+  const { wrapperRef, setOpen, setClose, getDropdown } = useDropdown();
+  return (
+    <div className="search" ref={wrapperRef}>
+      <input
+        placeholder="search"
+        onFocus={() => setOpen()}
+        ref={inputRef}
+        onChange={onChange}
+      />
+      {getDropdown(
+        <div>
+          {getSearchInfo()}
+        </div>
+      )}
     </div>
-}
+  );
+};
 
-export default Search
+export default Search;
